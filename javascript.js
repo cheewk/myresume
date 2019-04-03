fe89a3167b100f1a01b8c2b0602e0876b1bb8eab
cheewk <35590885+cheewk@users.noreply.github.com>
2019-04-03 17:22:57 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -6,6 +6,33 @@
 
 /*  ----- Javascript function() -----  */
 
+// == for modal picture == 
+ 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.myImg');
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 /*Scroll to top when arrow up clicked BEGIN*/
 
 if ($('#back-to-top').length) {
